db43c251d7378a779f8e6bc4545590f899844fc0
Pavlo Zahozhenko <pavlozahozhenko@gmail.com>
2021-07-15 16:11:53 +0300

    Check payload/key algorithm match among all supported algorithm aliases

--- lib/jwt/algos/eddsa.rb
+++ lib/jwt/algos/eddsa.rb
@@ -8,15 +8,22 @@ module JWT
       def sign(to_sign)
         algorithm, msg, key = to_sign.values
         raise EncodeError, "Key given is a #{key.class} but has to be an RbNaCl::Signatures::Ed25519::SigningKey" if key.class != RbNaCl::Signatures::Ed25519::SigningKey
-        raise IncorrectAlgorithm, "payload algorithm is #{algorithm} but #{key.primitive} signing key was provided"  if algorithm.downcase.to_sym != key.primitive
+        raise IncorrectAlgorithm, "payload algorithm is #{algorithm} but #{key.primitive} signing key was provided" unless valid_algorithm?(algorithm, key)
         key.sign(msg)
       end
 
       def verify(to_verify)
         algorithm, public_key, signing_input, signature = to_verify.values
-        raise IncorrectAlgorithm, "payload algorithm is #{algorithm} but #{public_key.primitive} verification key was provided" if algorithm.downcase.to_sym != public_key.primitive
+        raise IncorrectAlgorithm, "payload algorithm is #{algorithm} but #{public_key.primitive} verification key was provided" unless valid_algorithm?(algorithm, key)
         raise DecodeError, "key given is a #{public_key.class} but has to be a RbNaCl::Signatures::Ed25519::VerifyKey" if public_key.class != RbNaCl::Signatures::Ed25519::VerifyKey
         public_key.verify(signature, signing_input)
+      end
+
+      private
+
+      def valid_algorithm?(algorithm, key)
+        algorithm_aliases = SUPPORTED.map(&:to_sym)
+        algorithm.downcase.to_sym == key.primitive || (algorithm_aliases.include?(algorithm) && algorithm_aliases.map(&:downcase).include?(key.primitive))
       end
     end
   end
